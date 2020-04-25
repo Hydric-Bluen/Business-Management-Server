003ee19c717c8cf4d3ac197ccde9b4033b4bc829
Hydric-Bluen <myrpggaming@gmail.com>
2020-04-24 21:20:33 -0500

    Added in the employee list

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let open = false;
+const table = document.getElementById('employee-table');
 
+let open = false;
+let employees = ['Alyssa', 'Courtney', 'Harper', 'Johnson', 'Lance', 'Stronge'];
+
+// TODO: Make the top bar variables and continue working on making the employee loading system easier to manage.
+
+// Parse Employee Information
+function parseEmployeeInformation() {
+  for(employee in employees) {
+    table.innerHTML += `<tr><td><p>${employees[employee].toString()}</p></td></tr>`;
+  }
+}
+
+// Open and close the different windows in the area.
 function displayWindow(value) {
   switch(open) {
     case true:
@@ -7,6 +20,12 @@ function displayWindow(value) {
       open = false;
     break;
     case false:
+      document.getElementById(`${value}`).style.display = 'inline-flex';
+      open = true;
     break;
   }
+}
+
+window.onload = function() {
+  parseEmployeeInformation();
 }
